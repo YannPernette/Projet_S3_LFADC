7bbf2af947720242d52a5f8861c10024bb214052
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-11-23 09:54:39 +0100

    some things i guess

---
+++ wp-content/themes/theme_lfadc/association.php
@@ -0,0 +1,17 @@
+<?php
+/* Template Name: Association */
+get_header();
+?>
+
+<div class="">
+
+    <div>
+        <h2>Qui sommes-nous ?</h2>
+        <p>sfhvsvo ohj ofheo e</p>
+    </div>
+
+    <div>
+        <p class="text-h2">dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+    </div>
+
+</div>
