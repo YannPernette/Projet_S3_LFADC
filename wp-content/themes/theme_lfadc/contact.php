3ff782582cd26932ce1faf8c05df449d19dbeabf
Saykoo <116798135+KylianRouillardGaertner@users.noreply.github.com>
2023-11-23 08:59:24 +0100

    Contact

---
+++ wp-content/themes/theme_lfadc/contact.php
@@ -0,0 +1,33 @@
+<?php
+/*
+Template Name: Contact
+*/
+
+get_header();
+?>
+<div>
+    <div>
+        <h1></h1>
+        <h3></h3>
+        <h3></h3>
+    </div>
+    <div>
+        <h2></h2>
+        <p></p>
+        <p></p>
+        <div>
+            <h2></h2>
+            <form action="/contact.php">
+                <div><label for=""></label><input type="text"></div>
+                <div><label for=""></label><input type="text"></div>
+                <div><label for=""></label><input type="text"></div>
+                <div><label for=""></label><input type="text"></div>
+                <div><label for=""></label><input type="text"></div>
+                <div><input type="checkbox" name="" value=""><label for=""></label></div>
+                <input type="submit" value="SubmitContact">
+            </form>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
