4cf26e8c558de906b5713a1bb7dd1087bb62939c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-11-23 10:55:20 +0100

    commencement page asso

--- wp-content/themes/theme_lfadc/association.php
+++ wp-content/themes/theme_lfadc/association.php
@@ -5,15 +5,51 @@ get_header();
 
 <div class="bg-beige font-montserrat">
 
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2 mx-32">
 
         <div>
-            <h2>Qui sommes-nous ?</h2>
-            <p>sfhvsvo ohj ofheo e</p>
+            <h2 class="text-h2">Qui sommes-nous ?</h2>
+            <p>L’association est née en mars 2023 de la volonté de mener des actions de médiation climatique.
+                Les premières actions datent de 2021 et 2022, avec le projet “2050 Look UP!” à destination des scolaires et étudiants, une initiative portée par Daniel Gilbert, professeur en écologie. Basée dans le Doubs, l’activité se fait actuellement à l’échelle de la Franche-Comté, en Suisse et peut-être chez vous dès demain !</p>
+            <p>L’association est administrée pour l’année 2023-2024 par les membres bénévoles : 
+                Président : Daniel Gilbert 
+                Vice-président : Philippe Binet
+                Secrétaire : Patrick Ferrandiz
+                Trésorière : Anne-Sophie Berthet 
+                et autres membres fondateurs :
+                Mathias Bieber-Vagneux
+                Aurélie Carré
+                Anne Cheneau
+                Hélène Magnin-Feysot</p>
         </div>
 
         <div>
-            <p class="text-h2">dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+            <p>dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+        </div>
+
+    </div>
+
+
+    <div class="grid grid-cols-2 mx-32">
+
+        <div>
+            <p>dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+        </div>
+
+        <div>
+            <h2 class="text-h2">Nos missions</h2>
+            <p>L’association est née en mars 2023 de la volonté de mener des actions de médiation climatique.
+                Les premières actions datent de 2021 et 2022, avec le projet “2050 Look UP!” à destination des scolaires et étudiants, une initiative portée par Daniel Gilbert, professeur en écologie. Basée dans le Doubs, l’activité se fait actuellement à l’échelle de la Franche-Comté, en Suisse et peut-être chez vous dès demain !</p>
+            <p>L’association est administrée pour l’année 2023-2024 par les membres bénévoles : 
+                Président : Daniel Gilbert 
+                Vice-président : Philippe Binet
+                Secrétaire : Patrick Ferrandiz
+                Trésorière : Anne-Sophie Berthet 
+                et autres membres fondateurs :
+                Mathias Bieber-Vagneux
+                Aurélie Carré
+                Anne Cheneau
+                Hélène Magnin-Feysot</p>
         </div>
 
     </div>
