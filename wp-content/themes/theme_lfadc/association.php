911dbc844f4e2f2faa6c38352f2c3783f11bb23c
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-11-30 09:40:19 +0100

    test images

--- wp-content/themes/theme_lfadc/association.php
+++ wp-content/themes/theme_lfadc/association.php
@@ -3,12 +3,17 @@
 get_header();
 ?>
 
-<div class="bg-beige font-montserrat">
+<div class="bg-beige font-montserrat px-32">
 
-    <div class="grid grid-cols-2 mx-32">
+
+    <div class="bg-vert-clair py-20 my-12 text-h5 font-bold flex justify-center">
+    </div>
+
+
+    <div class="grid grid-cols-2 mb-20">
 
         <div>
-            <h2 class="text-h2">Qui sommes-nous ?</h2>
+            <h2 class="text-h2 font-bold">Qui sommes-nous ?</h2>
             <p>L’association est née en mars 2023 de la volonté de mener des actions de médiation climatique.
                 Les premières actions datent de 2021 et 2022, avec le projet “2050 Look UP!” à destination des scolaires et étudiants, une initiative portée par Daniel Gilbert, professeur en écologie. Basée dans le Doubs, l’activité se fait actuellement à l’échelle de la Franche-Comté, en Suisse et peut-être chez vous dès demain !</p>
             <p>L’association est administrée pour l’année 2023-2024 par les membres bénévoles : 
@@ -30,6 +35,7 @@ get_header();
     </div>
 
 
+    <h2 class="text-h2 font-bold">Nos missions</h2>
     <div class="grid grid-cols-2 mx-32">
 
         <div>
@@ -37,21 +43,39 @@ get_header();
         </div>
 
         <div>
-            <h2 class="text-h2">Nos missions</h2>
-            <p>L’association est née en mars 2023 de la volonté de mener des actions de médiation climatique.
-                Les premières actions datent de 2021 et 2022, avec le projet “2050 Look UP!” à destination des scolaires et étudiants, une initiative portée par Daniel Gilbert, professeur en écologie. Basée dans le Doubs, l’activité se fait actuellement à l’échelle de la Franche-Comté, en Suisse et peut-être chez vous dès demain !</p>
-            <p>L’association est administrée pour l’année 2023-2024 par les membres bénévoles : 
-                Président : Daniel Gilbert 
-                Vice-président : Philippe Binet
-                Secrétaire : Patrick Ferrandiz
-                Trésorière : Anne-Sophie Berthet 
-                et autres membres fondateurs :
-                Mathias Bieber-Vagneux
-                Aurélie Carré
-                Anne Cheneau
-                Hélène Magnin-Feysot</p>
+            <h3 class="text-h3 font-bold text-bleu">Proposer des solutions actives</h3>
+            <p>Tout au long de l'année, l'association organise des évènements imaginés avec les jeunes et les moins jeunes en collaboration avec des acteurs locaux. Des expositions, des conférences, des spectacles... pour construire ensemble un futur désirable.</p>
         </div>
 
+
+        <div>
+            <h3 class="text-h3 font-bold text-bleu">Construire un avenir ensemble</h3>
+            <p>Entre actions individuelles et décisions de l’État, il existe toute une série de possibilités à l'échelle des collectifs locaux. Écoles, universités, collectivités, associations, entreprises... et si la solution se trouvait aussi à un niveau intermédiaire ?</p>
+        </div>
+
+        <div>
+            <p>dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+        </div>
+
+
+        <div>
+            <p>dwbjhvgswugv iyvgikyvkwvyhukgsrjugyusrvgysvgyuisygvykiv</p>
+        </div>
+
+        <div>
+            <h3 class="text-h3 font-bold text-bleu">Proposer des activités ludiques et créatives</h3>
+            <p>"Le futur a déjà commencé" propose des ateliers ludiques pour laisser libre cours à son imagination et proposer des solutions en adéquation avec les enjeux climatiques.</p>
+        </div>
+
+    </div>
+
+
+    <div class="bg-vert-clair py-20 my-12 text-h5 font-bold flex justify-center">
+        <div class="text-bleu">
+            <p class="mb-0">Notre association vous intéresse ?</p>
+            <p class="mb-6">Envie de proposer un projet ou d’y participer ?</p>
+            <p>Vous pouvez devenir membre de notre association, remplissez ce formulaire ici</p>
+        </div>
     </div>
 
 </div>
