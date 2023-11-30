87701d0c5066bece3797418f9ff683c1cc9b1bec
YannPernette <yann.pernette@edu.univ-fcomte.fr>
2023-11-30 10:31:12 +0100

    amelioration functions

--- wp-content/themes/theme_lfadc/functions.php
+++ wp-content/themes/theme_lfadc/functions.php
@@ -1,5 +1,16 @@
-<!-- Code exécuté à chaque fois que le thème est chargé -->
+<?php add_theme_support( 'post-thumbnails' ); ?>
 
+<?php 
+    //ajouter une nouvelle zone de menu à mon thème
+    function register_my_menu(){
+        register_nav_menus( array(
+            'header-menu' => __( 'Menu header'),
+            'footer-menu'  => __( 'Menu footer'),
+        ) );
+    }
+    add_action( 'init', 'register_my_menu', 0 );
+   
+?>
 <?php
 function montheme_supports() {
     add_theme_support('title-tag');
