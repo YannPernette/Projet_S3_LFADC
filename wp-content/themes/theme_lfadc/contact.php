c318e4f635013d0b2fb76acf988e7b03da88fb8f
Saykoo <116798135+KylianRouillardGaertner@users.noreply.github.com>
2023-12-14 11:34:19 +0100

    change font size

--- wp-content/themes/theme_lfadc/contact.php
+++ wp-content/themes/theme_lfadc/contact.php
@@ -5,10 +5,10 @@ Template Name: Contact
 
 get_header();
 ?>
-<div class="bg-beige grid grid-rows-[1fr] text-bleu text-body">
+<div class="bg-beige grid grid-rows-[1fr] text-bleu sm:text-body_mobile text-body">
     <div class="text-center font-dela-gothic-one relative overflow-hidden h-[19rem]">
         <img src="http://localhost/LFADC/wp-content/uploads/2023/12/climat-scaled-1.webp" alt="banner img" class="object-cover w-full h-full" style="object-position: 50% 69%;">
-        <div class="absolute top-0 left-0 w-full h-full opacity-50 bg-blanc z-10"></div>
+        <div class="absolute top-0 left-0 w-full h-full opacity-60 bg-blanc z-10"></div>
         <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-20 w-full">
             <h1 class="sm:text-h1_mobile text-h1">
                 <?php the_title(); ?>
@@ -26,21 +26,21 @@ get_header();
         <div class="text-noir mb-10">
             <h2 class='sm:text-h2_mobile text-h2 text-bleu mb-6 mt-0 font-dela-gothic-one'>Voici comment nous contacter</h2>
             <p class="mb-0 font-montserrat">Vous avez une question précise, vous avez envie de discuter avec les membres de l’association pour aller plus loin, 
-                vous avez un projet à nous proposer, envoyez-nous un mail à <a href="mailto:lefuturadejacommence@gmail.com">lefuturadejacommence@gmail.com</a></p>
-                Vous pouvez également nous envoyer votre question par formulaire
+                vous avez un projet à nous proposer, envoyez-nous un mail à <a href="mailto:lefuturadejacommence@gmail.com">lefuturadejacommence@gmail.com</a></br>
+                Vous pouvez également nous envoyer votre question par formulaire</p>
         </div>
         <div class='bg-blanc mb-10 p-4 md:px-3 lg:px-14 px-32 rounded-lg shadow-md'>
             <h2 class="sm:text-h2_mobile text-h2 text-center mb-16 mt-4 font-dela-gothic-one">Formulaire de contact</h2>
             <form class="font-montserrat" action="/contact.php">
                 <div class="grid grid-cols-2 gap-5 my-4">
-                    <div><label style="margin-bottom: 0.5rem;" for="lastname">Nom</label><input class="h-12" name="lastname" placeholder="Entrez votre nom" type="text"></div>
-                    <div><label style="margin-bottom: 0.5rem;" for="surname">Prénom</label><input class="h-12" name="surname" placeholder="Entrez votre prénom" type="text"></div>
+                    <div><label class="mb-2" for="lastname">Nom</label><input class="h-12" name="lastname" placeholder="Entrez votre nom" type="text"></div>
+                    <div><label class="mb-2" for="surname">Prénom</label><input class="h-12" name="surname" placeholder="Entrez votre prénom" type="text"></div>
                 </div>
-                <div class="my-4"><label style="margin-bottom: 0.5rem;" for="mail">Email</label><input class="h-12" name="mail" placeholder="Entrez votre Email" type="email"></div>
-                <div class="my-4"><label style="margin-bottom: 0.5rem;" for="object">Objet du message</label><input class="h-12" name="object" placeholder="Entrez votre sujet" type="text"></div>
-                <div class="my-4"><label style="margin-bottom: 0.5rem;" for="content">Commentaire du Message</label><textarea name="content" id="" placeholder="Entrez votre message" rows="6"></textarea></div>
+                <div class="my-4"><label class="mb-2" for="mail">Email</label><input class="h-12" name="mail" placeholder="Entrez votre Email" type="email"></div>
+                <div class="my-4"><label class="mb-2" for="object">Objet du message</label><input class="h-12" name="object" placeholder="Entrez votre sujet" type="text"></div>
+                <div class="my-4"><label class="mb-2" for="content">Commentaire du Message</label><textarea name="content" id="" placeholder="Entrez votre message" rows="6"></textarea></div>
                 <div class='flex my-4'><input class='self-start mr-1' type="checkbox" name="MentionLegal" value=""><label for="MentionLegal">En envoyant ce formulaire, j'accepte que les informations saisies soient utilisées aux fins de traitement ou de prise de contact avec la personne concernée. Il n’y a pas de transfert de ces données personnelles à des tiers.</label></div>
-                <input class="!bg-orange hover:!bg-[#B85227] !w-full !text-blanc !border-none !rounded-lg mb-7 shadow-md" type="submit" value="Envoyer" >
+                <input class="!bg-orange hover:!bg-[#B85227] !w-full !text-blanc !border-none !rounded-lg !font-semibold mb-7 shadow-md" type="submit" value="ENVOYER" >
             </form>
         </div>
     </div>
--- wp-content/themes/theme_lfadc/tailwind.config.js
+++ wp-content/themes/theme_lfadc/tailwind.config.js
@@ -16,23 +16,23 @@ module.exports = {
       "montserrat": ['Montserrat', 'sans-serif'],
     },
     fontSize: {
-      body: '0.875rem',            // Taille pour laptop (14px)
-      'body_mobile': '0.7rem',  // Taille pour mobile (14px / 16px = 0.875 * 0.9 = 0.7875rem)
-
-      h5: '1rem',                  // Taille pour laptop (16px)
-      'h5_mobile': '0.8rem',       // Taille pour mobile (16px / 16px = 1 * 0.9 = 0.9rem)
-
-      h4: '1.125rem',              // Taille pour laptop (18px)
-      'h4_mobile': '0.9rem',    // Taille pour mobile (18px / 16px = 1.125 * 0.9 = 1.0125rem)
-
-      h3: '1.375rem',              // Taille pour laptop (22px)
-      'h3_mobile': '1.1rem',    // Taille pour mobile (22px / 16px = 1.375 * 0.9 = 1.2375rem)
-
-      h2: '1.625rem',              // Taille pour laptop (26px)
-      'h2_mobile': '1.3rem',    // Taille pour mobile (26px / 16px = 1.625 * 0.9 = 1.4625rem)
-
-      h1: '2rem',                  // Taille pour laptop (32px)
-      'h1_mobile': '1.6rem',       // Taille pour mobile (32px / 16px = 2 * 0.9 = 1.8rem)
+      body: '1rem',               // Taille pour laptop (16px)
+      'body_mobile': '0.875rem',  // Taille pour mobile (16px * 0.875 = 14px)
+    
+      h5: '1.125rem',          // Taille pour laptop (18px)
+      'h5_mobile': '0.9rem',   // Taille pour mobile (18px * 0.9 = 16.2px)
+    
+      h4: '1.375rem',          // Taille pour laptop (22px)      
+      'h4_mobile': '1.125rem', // Taille pour mobile (22px * 0.9 = 19.8px)
+    
+      h3: '1.5rem',            // Taille pour laptop (24px)
+      'h3_mobile': '1.2rem',   // Taille pour mobile (24px * 0.9 = 21.6px)
+    
+      h2: '1.625rem',          // Taille pour laptop (26px)
+      'h2_mobile': '1.375rem',   // Taille pour mobile (26px * 0.9 = 23.4px)
+    
+      h1: '2rem',              // Taille pour laptop (32px)
+      'h1_mobile': '1.6rem',   // Taille pour mobile (32px * 0.9 = 28.8px)
     },
     screens: {
       'xl': {'max': '1439px'},
